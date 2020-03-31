d2bb6f56055352f2a5b75df172e9fb8f6a63bf9c
mitchavery <avery.mitch651@gmail.com>
2020-03-30 22:36:21 -0700

    init function

--- app.js
+++ app.js
@@ -27,22 +27,31 @@ var UIController = (function() {
 
 //Global App Controller
 var controller = (function(budgetCtrl, UICtrl) {
-  var DOM = UICtrl.getDomstrings();
+  var setupEventListeners = function() {
+    var DOM = UICtrl.getDomstrings();
+    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
+    document.addEventListener("keypress", function(event) {
+      if (event.key === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
   var ctrlAddItem = function() {
     //1. Get the filed input data
     var input = UICtrl.getInput();
-    console.log(input);
     //2. Add the item to the budget controller
     //3. Add the item to the UI
     //4. Calculate the budget
     //5. Display the budget
   };
 
-  document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
-
-  document.addEventListener("keypress", function(event) {
-    if (event.key === 13 || event.which === 13) {
-      ctrlAddItem();
+  return {
+    init: function() {
+      console.log("App has started");
+      setupEventListeners();
     }
-  });
+  };
 })(budgetController, UIController);
+
+controller.init();

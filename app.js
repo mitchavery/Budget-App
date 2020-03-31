f5e5adf8c5894c238a4e538db3a19732f1200462
mitchavery <avery.mitch651@gmail.com>
2020-03-31 16:19:17 -0700

    cleared input fields

--- app.js
+++ app.js
@@ -88,6 +88,20 @@ var UIController = (function() {
       document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
     },
 
+    clearFields: function() {
+      var fields, fieldsArr;
+      fields = document.querySelectorAll(
+        DOMstrings.inputDescription + "," + DOMstrings.inputValue
+      );
+
+      fieldsArr = Array.prototype.slice.call(fields);
+      fieldsArr.forEach(function(current, index, array) {
+        current.value = "";
+      });
+
+      fieldsArr[0].focus();
+    },
+
     getDomstrings: function() {
       return DOMstrings;
     }
@@ -114,7 +128,8 @@ var controller = (function(budgetCtrl, UICtrl) {
     newItem = budgetCtrl.addItem(input.type, input.description, input.value);
     //3. Add the item to the UI
     UICtrl.addListItem(newItem, input.type);
-
+    //4. Clear the fields
+    UICtrl.clearFields();
     //4. Calculate the budget
     //5. Display the budget
   };

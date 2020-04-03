18ef201f5716c9c16fbde8f666d9024c6b2d64d9
mitchavery <avery.mitch651@gmail.com>
2020-04-03 16:38:06 -0700

    UICtrl add

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ var budgetController = (function() {
   Expense.prototype.getPercentage = function() {
     return this.percentage;
   };
-  
 
   var Income = function(id, description, value) {
     this.id = id;
@@ -70,12 +69,6 @@ var budgetController = (function() {
 
     deleteItem: function(type, id) {
       var ids, index;
-
-      // id = 6
-      //data.allItems[type][id];
-      // ids = [1 2 4  8]
-      //index = 3
-
       ids = data.allItems[type].map(function(current) {
         return current.id;
       });
@@ -88,34 +81,19 @@ var budgetController = (function() {
     },
 
     calculateBudget: function() {
-      // calculate total income and expenses
       calculateTotal("exp");
       calculateTotal("inc");
 
-      // Calculate the budget: income - expenses
       data.budget = data.totals.inc - data.totals.exp;
 
-      // calculate the percentage of income that we spent
       if (data.totals.inc > 0) {
         data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
       } else {
         data.percentage = -1;
       }
-
-      // Expense = 100 and income 300, spent 33.333% = 100/300 = 0.3333 * 100
     },
 
     calculatePercentages: function() {
-      /*
-          a=20
-          b=10
-          c=40
-          income = 100
-          a=20/100=20%
-          b=10/100=10%
-          c=40/100=40%
-          */
-
       data.allItems.exp.forEach(function(cur) {
         cur.calcPercentage(data.totals.inc);
       });
@@ -143,7 +121,6 @@ var budgetController = (function() {
   };
 })();
 
-// UI CONTROLLER
 var UIController = (function() {
   var DOMstrings = {
     inputType: ".add__type",
@@ -163,15 +140,6 @@ var UIController = (function() {
 
   var formatNumber = function(num, type) {
     var numSplit, int, dec, type;
-    /*
-          + or - before number
-          exactly 2 decimal points
-          comma separating the thousands
-
-          2310.4567 -> + 2,310.46
-          2000 -> + 2,000.00
-          */
-
     num = Math.abs(num);
     num = num.toFixed(2);
 
@@ -179,7 +147,7 @@ var UIController = (function() {
 
     int = numSplit[0];
     if (int.length > 3) {
-      int = int.substr(0, int.length - 3) + "," + int.substr(int.length - 3, 3); //input 23510, output 23,510
+      int = int.substr(0, int.length - 3) + "," + int.substr(int.length - 3, 3);
     }
 
     dec = numSplit[1];
@@ -196,7 +164,7 @@ var UIController = (function() {
   return {
     getInput: function() {
       return {
-        type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+        type: document.querySelector(DOMstrings.inputType).value,
         description: document.querySelector(DOMstrings.inputDescription).value,
         value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
       };
@@ -204,7 +172,6 @@ var UIController = (function() {
 
     addListItem: function(obj, type) {
       var html, newHtml, element;
-      // Create HTML string with placeholder text
 
       if (type === "inc") {
         element = DOMstrings.incomeContainer;
@@ -218,12 +185,10 @@ var UIController = (function() {
           '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
       }
 
-      // Replace the placeholder text with some actual data
       newHtml = html.replace("%id%", obj.id);
       newHtml = newHtml.replace("%description%", obj.description);
       newHtml = newHtml.replace("%value%", formatNumber(obj.value, type));
 
-      // Insert the HTML into the DOM
       document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
     },
 
@@ -288,7 +253,6 @@ var UIController = (function() {
       var now, months, month, year;
 
       now = new Date();
-      //var christmas = new Date(2016, 11, 25);
 
       months = [
         "January",
@@ -333,7 +297,6 @@ var UIController = (function() {
   };
 })();
 
-// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
   var setupEventListeners = function() {
     var DOM = UICtrl.getDOMstrings();
@@ -356,47 +319,35 @@ var controller = (function(budgetCtrl, UICtrl) {
   };
 
   var updateBudget = function() {
-    // 1. Calculate the budget
     budgetCtrl.calculateBudget();
 
-    // 2. Return the budget
     var budget = budgetCtrl.getBudget();
 
-    // 3. Display the budget on the UI
     UICtrl.displayBudget(budget);
   };
 
   var updatePercentages = function() {
-    // 1. Calculate percentages
     budgetCtrl.calculatePercentages();
 
-    // 2. Read percentages from the budget controller
     var percentages = budgetCtrl.getPercentages();
 
-    // 3. Update the UI with the new percentages
     UICtrl.displayPercentages(percentages);
   };
 
   var ctrlAddItem = function() {
     var input, newItem;
 
-    // 1. Get the field input data
     input = UICtrl.getInput();
 
     if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
-      // 2. Add the item to the budget controller
       newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
-      // 3. Add the item to the UI
       UICtrl.addListItem(newItem, input.type);
 
-      // 4. Clear the fields
       UICtrl.clearFields();
 
-      // 5. Calculate and update budget
       updateBudget();
 
-      // 6. Calculate and update percentages
       updatePercentages();
     }
   };
@@ -412,16 +363,12 @@ var controller = (function(budgetCtrl, UICtrl) {
       type = splitID[0];
       ID = parseInt(splitID[1]);
 
-      // 1. delete the item from the data structure
       budgetCtrl.deleteItem(type, ID);
 
-      // 2. Delete the item from the UI
       UICtrl.deleteListItem(itemID);
 
-      // 3. Update and show the new budget
       updateBudget();
 
-      // 4. Calculate and update percentages
       updatePercentages();
     }
   };
